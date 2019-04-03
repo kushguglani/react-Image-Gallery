bd390b01b1ffa5ec54c9cb283e4944f3f7ce2f9f
kushguglani <kushguglani5@gmail.com>
2019-04-03 14:58:40 +0530

    Update unsplash.js

--- src/unsplash.js
+++ src/unsplash.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 export default axios.create({
 	baseURL: 'https://api.unsplash.com/',
 	headers: {
-		Authorization: 'Client-ID fb00f257d157d89c4858885882681974491606fa999fdbbf4e046f0eb6ae9b3a'
+		Authorization: 'Client-ID '
 		// Authorization: Client - ID YOUR_ACCESS_KEY
 	}
 })
